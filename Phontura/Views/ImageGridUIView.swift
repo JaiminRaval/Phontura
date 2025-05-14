357ffc8758956d09e0f20f4d3ea22f8fc5a0c3a5
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-14 23:59:18 +0530

    added gradient

--- Phontura/Views/ImageGridUIView.swift
+++ Phontura/Views/ImageGridUIView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ImageGridUIView: View {
     var body: some View{
         ZStack {
-            
+            LinearGradient(
+                           gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.purple.opacity(0.7)]),
+                           startPoint: .top,
+                           endPoint: .bottom
+                       )
+                       .ignoresSafeArea()
         }
     }
     
