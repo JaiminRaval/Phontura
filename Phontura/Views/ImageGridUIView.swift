ca8178bd072f70ce7461640922c704fb133266b7
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-14 23:55:09 +0530

    removed redundant code

--- Phontura/Views/ImageGridUIView.swift
+++ Phontura/Views/ImageGridUIView.swift
@@ -8,34 +8,12 @@
 import SwiftUI
 
 struct ImageGridUIView: View {
-    private let data: Data
-    private let spacing: CGFloat
-    private let columns: Int
-//    private let itemContent: (Data.Element) -> ItemView
-        
-        @State private var size: CGSize = .zero
-      var body: some View {
-          GeometryReader { geometry in
-              ScrollView {
-                  LazyVGrid(columns: gridItems(), spacing: spacing) {
-                      ForEach(data) { item in
-                          itemContent(item)
-                              // Set a fixed aspect ratio for consistent grid appearance
-                              .aspectRatio(1, contentMode: .fill)
-                              .frame(maxWidth: .infinity, maxHeight: .infinity)
-                              .clipped()
-                      }
-                  }
-                  .padding(.horizontal, spacing)
-                  .onAppear {
-                      size = geometry.size
-                  }
-                  .onChange(of: geometry.size) { newSize in
-                      size = newSize
-                  }
-              }
-          }
-      }
+    var body: some View{
+        ZStack {
+            
+        }
+    }
+    
 }
 
 #Preview {
